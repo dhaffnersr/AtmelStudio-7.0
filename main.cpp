05affde17cb40d4b35597764bd26521b3625e396
david h haffner Sr <dhaffnersr@gmail.com>
2017-04-30 18:52:05 -0400

    Add files via upload
    This is the Firmware for the TCD1304DG CCD/Atmega1284P chip that drives the CCD circuit for the 3D printable Raman Spectrometer @ Hackaday.com

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+ * TCD1304DG_drvAPR30.cpp
+ *
+ * Created: 4/30/2017 6:39:47 PM
+ * Author : dhaff
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
